8c90ee0fcb6269833a912c5af8474b7a6d7cdbdb
Jeongp4939 <jeongp12@gmail.com>
2023-10-19 11:50:56 +0900

    develop [feat] 소리에 따라 움직이는 블록 스크립트 작성

--- Game/Assets/MicSubscriber.cs
+++ Game/Assets/MicSubscriber.cs
@@ -8,6 +8,11 @@ public class MicSubscriber : MonoBehaviour
     MicInputManager input;
     public float minDB = -160;
     public float maxDB = 1000;
+    public float scaleFactor = 1.0f;
+
+    public Color normalColor = Color.green;
+    public Color bigSoundColor = Color.red;
+    private SpriteRenderer renderer;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +23,7 @@ public class MicSubscriber : MonoBehaviour
             setScale(0);
             return;
         }
+        renderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -27,14 +33,32 @@ public class MicSubscriber : MonoBehaviour
         {
             return;
         }
-        setScale(getDBRatio(input.DB) * 0.25f);
+        if (input.DB < minDB)
+        {
+            setScale(0);
+            setColor(normalColor);
+        }
+        else if (input.DB > maxDB)
+        {
+            setScale(1);
+            setColor(bigSoundColor);
+        }
+        else
+        {
+            setScale(getDBRatio(input.DB));
+            setColor(normalColor);
+        }
     }
     void setScale(float scale)
     {
-        transform.localScale = new Vector3(scale, scale, scale);
+        transform.localScale = new Vector3(scale, scale, scale) * scaleFactor;
     }
     float getDBRatio(float db)
     {
         return (db - minDB) / (maxDB - minDB);
+    }
+    void setColor(Color color)
+    {
+        renderer.color = color;
     }
 }
