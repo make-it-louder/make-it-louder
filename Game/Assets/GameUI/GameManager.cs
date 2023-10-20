63aab21077d6ee92fe673c937f00a98916da3f99
ungseo <ungseo@gmail.com>
2023-10-20 17:53:10 +0900

    [DESIGN] 로그인폼 디자인

--- Game/Assets/GameUI/GameManager.cs
+++ Game/Assets/GameUI/GameManager.cs
@@ -7,10 +7,15 @@ public class GameManager : MonoBehaviour
 {
     public TMP_InputField emailInputField;
     public TMP_InputField passwordInputField;
+    private GameObject loginForm;
+    private GameObject signupForm;
     // Start is called before the first frame update
     void Start()
     {
-        
+        loginForm = GameObject.Find("LoginForm");
+        signupForm = GameObject.Find("SignUpForm");
+        loginForm.SetActive(true);
+        signupForm.SetActive(false);
     }
 
     // Update is called once per frame
@@ -18,14 +23,17 @@ public class GameManager : MonoBehaviour
     {
         
     }
-    public void GoToScene(string sceneName) => SceneManager.LoadScene(sceneName);
+    //회원가입폼으로 변경
+    public void GoToSignup()
+    {
+        signupForm.SetActive(true);
+        loginForm.SetActive(false);
+    }
 
 
     //로그인
     public void Login()
     {
-
-
         SceneManager.LoadScene("Lobby");
     }
 }
