6aefaedabba20925586a9a08b417797008b79458
Jeongp4939 <jeongp12@gmail.com>
2023-10-24 12:04:02 +0900

    S09P31C102-98 [Feat] CreateRatateObstacle

---
+++ Assets/Game/Scripts/RotateObstacle.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObstacle : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+
+    public float rotateSpeed = 0.2f;
+    public bool reverse;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!reverse)
+        {
+            transform.Rotate(0, 0, rotateSpeed);
+        }
+        else
+        {
+            transform.Rotate(0, 0, -rotateSpeed);
+        }
+    }
+}
