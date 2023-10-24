2b828306748a6c120ecdcb224daefa8958f919c6
ungseo <ungseo@gmail.com>
2023-10-24 23:24:31 +0900

    S09P31C102-89 [Feat] 다운로드 웹 추가

---
+++ homepage/src/App.tsx
@@ -0,0 +1,53 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="nav-bar">
+        <button>
+          Download <img src="/downloads.png" alt="" />
+        </button>
+      </div>
+
+      <div className="container">
+        <div className="heaven">
+          <img src="/c.png" alt="" />
+          <img src="/c.png" alt="" />
+          <img src="/c.png" alt="" />
+          <img src="/c.png" alt="" />
+          <img src="/c.png" alt="" />
+          <img src="/c.png" alt="" />
+        </div>
+        <div className="angels">
+          <div className="ang">
+            <img src="/ang1.png" alt="" />
+            <img src="/ang2.png" alt="" />
+          </div>
+        </div>
+        <div className="middle">
+          <img src="logo.png" alt="" />
+        </div>
+        <div className="devils">
+          <div className="dev">
+            <img src="/dev1.png" alt="" />
+            <img src="/dev2.png" alt="" />
+          </div>
+        </div>
+        <div className="hell">
+          <img src="/f.png" alt="" />
+          <img src="/f.png" alt="" />
+          <img src="/f.png" alt="" />
+          <img src="/f.png" alt="" />
+          <img src="/f.png" alt="" />
+          <img src="/f.png" alt="" />
+          <img src="/f.png" alt="" />
+          <img src="/f.png" alt="" />
+          <img src="/f.png" alt="" />
+          <img src="/f.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
