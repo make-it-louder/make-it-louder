95851de6145f66773c67c47b53cd5db26589011d
ungseo <ungseo@gmail.com>
2023-10-26 15:15:55 +0900

    [Feat] Add FixedUI in MakeItLouder

--- Assets/GameUI/Scenes/Script/DisplayManager.cs
+++ Assets/GameUI/Scenes/Script/DisplayManager.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class DisplayManager : MonoBehaviour
 {
+    public Slider brightnessSlider;
+
     // Start is called before the first frame update
     void Start()
     {
