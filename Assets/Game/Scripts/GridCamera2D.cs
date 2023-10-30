cb84090fe7b3981d271f91b2934ac29624f52542
dyddnals@naver.com <dyddnals@naver.com>
2023-10-30 11:22:22 +0900

    [Feat] camera follow player

--- Assets/Game/Scripts/GridCamera2D.cs
+++ Assets/Game/Scripts/GridCamera2D.cs
@@ -7,12 +7,17 @@ public class GridCamera2D : MonoBehaviour
     [SerializeField]
     public GameObject follows;
     [SerializeField]
-    private Vector2 gridScale;
-    [SerializeField]
     private float depth = -10;
     [SerializeField]
     [Range(1, 10)]
     private float transitionSpeed;
+    [SerializeField]
+    [Range(0, 1)]
+    private float horizontalThreshold = 0.33f;  // Set a threshold for horizontal distance
+    [SerializeField]
+    [Range(0, 1)]
+    private float verticalThreshold = 0.33f;  // Set a threshold for vertical distance
+
     void Start()
     {
         if (follows == null)
@@ -21,21 +26,22 @@ public class GridCamera2D : MonoBehaviour
         }
     }
 
-    Vector3 GridIdx2Pos(Vector2Int gridIdx)
+    bool IsWithinCenterRegion(Vector3 position)
     {
-        Vector3 result;
-        result.y = gridIdx.y * gridScale.y;
-        result.x = gridIdx.x * gridScale.x;
-        result.z = depth;
-        return result;
+        // Adjusting the size of the center region based on horizontal and vertical thresholds
+        float horizontalLimit = Camera.main.orthographicSize * Camera.main.aspect * horizontalThreshold;
+        float verticalLimit = Camera.main.orthographicSize * verticalThreshold;
+
+        return Mathf.Abs(position.x - transform.position.x) < horizontalLimit &&
+               Mathf.Abs(position.y - transform.position.y) < verticalLimit;
     }
-    Vector2Int Pos2GridIdx(Vector3 pos)
+
+    bool IsOutsideViewport(Vector3 position)
     {
-        Vector2Int gridIdx = new Vector2Int();
-        gridIdx.x = (int)Mathf.Round(pos.x / gridScale.x);
-        gridIdx.y = (int)Mathf.Round(pos.y / gridScale.y);
-        return gridIdx;
+        Vector3 viewportPosition = Camera.main.WorldToViewportPoint(position);
+        return viewportPosition.y < 0 || viewportPosition.y > 1;
     }
+
     // Update is called once per frame
     void Update()
     {
@@ -43,8 +49,23 @@ public class GridCamera2D : MonoBehaviour
         {
             return;
         }
-        Vector2Int targetGridIdx = Pos2GridIdx(follows.transform.position);
-        Vector3 targetPos = GridIdx2Pos(targetGridIdx);
-        transform.position = Vector3.Lerp(transform.position, targetPos, transitionSpeed * Time.deltaTime);
+
+        if (IsWithinCenterRegion(follows.transform.position))
+        {
+            return;  // Do not move the camera if player is within the center region
+        }
+
+        if (IsOutsideViewport(follows.transform.position))
+        {
+            Vector3 targetPos = transform.position;  // Get the current camera position
+            targetPos.y = follows.transform.position.y + Camera.main.orthographicSize;  // Adjust only the Y position so the character is at the bottom of the screen
+            targetPos.z = depth;  // Set the target z-coordinate to the desired depth
+            transform.position = targetPos;  // Move the camera immediately to the target position
+            return;
+        }
+
+        Vector3 targetPosition = follows.transform.position;
+        targetPosition.z = depth;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, transitionSpeed * Time.deltaTime);
     }
 }
