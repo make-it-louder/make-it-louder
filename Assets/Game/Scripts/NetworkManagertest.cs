2aca3d9b35a756aee683d762bb56cfc84cec51fd
dyddnals@naver.com <dyddnals@naver.com>
2023-10-31 22:58:50 +0900

    [feat] Apply photon room list to lobby

--- Assets/Game/Scripts/NetworkManagertest.cs
+++ Assets/Game/Scripts/NetworkManagertest.cs
@@ -1,20 +1,24 @@
 using Photon.Pun;
 using Photon.Realtime;
 using System.Collections;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class NicknameManager : MonoBehaviourPunCallbacks
+public class NetworkManager : MonoBehaviourPunCallbacks
 {
     public TMP_InputField nicknameInputField;
     public GameObject playerPrefab;
+
+    public List<RoomInfo> myList { get; set; }
     private void Start()
     {
         DontDestroyOnLoad(gameObject);
         SceneManager.sceneLoaded += OnSceneLoaded;
+        myList = new List<RoomInfo>();
     }
     public void OnConnectButtonClicked()
     {
@@ -24,12 +28,23 @@ public class NicknameManager : MonoBehaviourPunCallbacks
             PhotonNetwork.AutomaticallySyncScene = true;
             PhotonNetwork.ConnectUsingSettings();
         }
+        SceneManager.LoadScene(2);
     }
 
     public override void OnConnectedToMaster()
     {
         Debug.Log("OnConnectedToMaster");
-        PhotonNetwork.JoinOrCreateRoom("Roomrr", new RoomOptions { MaxPlayers = 6 }, null); // ������ �濡 ����
+        PhotonNetwork.JoinLobby();
+        //PhotonNetwork.JoinOrCreateRoom("Roomrr", new RoomOptions { MaxPlayers = 6 }, null);
+    }
+
+    public void JoinRoom(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName, null);
+    }
+    public void CreateRoom(string roomName)
+    {
+        PhotonNetwork.CreateRoom(roomName,new RoomOptions { MaxPlayers = 6 }, null);
     }
 
     public override void OnJoinedRoom()
@@ -46,6 +61,23 @@ public class NicknameManager : MonoBehaviourPunCallbacks
         Debug.Log("Failed to join a random room. Creating a new room...");
         PhotonNetwork.CreateRoom(null);
     }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        int roomCount = roomList.Count;
+        for (int i = 0; i < roomCount; i++)
+        {
+            if (!roomList[i].RemovedFromList)
+            {
+                if (!myList.Contains(roomList[i])) myList.Add(roomList[i]);
+                else myList[myList.IndexOf(roomList[i])] = roomList[i];
+            }
+            else if (myList.IndexOf(roomList[i]) != -1) myList.RemoveAt(myList.IndexOf(roomList[i]));
+        }
+
+        RoomManager roomManager = GameObject.Find("RoomManager").GetComponent<RoomManager>();
+        if (roomManager != null) roomManager.UpdateRoomList();
+    }
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         Debug.Log("OnSceneLoaded");
@@ -56,7 +88,6 @@ public class NicknameManager : MonoBehaviourPunCallbacks
             StartCoroutine(OnScene3Loaded());
         }
     }
-
     private void OnDestroy()
     {
         // ���� ������Ʈ�� �ı��� �� �̺�Ʈ���� �޼��带 ����
--- Assets/GameUI/Script/RoomManager.cs
+++ Assets/GameUI/Script/RoomManager.cs
@@ -1,6 +1,8 @@
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +10,7 @@ public class RoomManager : MonoBehaviour
 {
     public TMP_InputField createName;
     public GameObject room;
+    public GameObject content;
     public string curNo { get; set; }
     public string curTitle { get; set; }
     private GameObject prevRoom;
@@ -16,6 +19,7 @@ public class RoomManager : MonoBehaviour
     void Start()
     {
         networkManager = GameObject.FindObjectOfType<NetworkManager>();
+        UpdateRoomList();
     }
 
     // Update is called once per frame
@@ -24,13 +28,35 @@ public class RoomManager : MonoBehaviour
         
     }
 
+    void DestroyAllChildren(GameObject parent)
+    {
+        foreach (Transform child in parent.transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+    public void UpdateRoomList()
+    {
+        DestroyAllChildren(content);
+        int i = 1;
+        foreach (RoomInfo roomInfo in networkManager.myList)
+        {
+            // Instantiate a new object
+            GameObject spawnedObject = Instantiate(room);
+            spawnedObject.transform.Find("No").GetComponent<TMP_Text>().text = i++.ToString("D3");
+            spawnedObject.transform.Find("Title").GetComponent<TMP_Text>().text = roomInfo.Name;
+
+            // Set the parent of the spawned object to the specified parent object
+            spawnedObject.transform.SetParent(content.transform);
+        }
+    }
     public void onClickCreate()
     {
-
+        networkManager.CreateRoom(createName.text);
     }
     public void onClickJoin()
     {
-
+        networkManager.JoinRoom(curTitle);
     }
     public void ChangeRoom(GameObject room)
     {
