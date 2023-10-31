02f936516913db1324b712baeede2e6d014c2058
arcto19 <arctophile19@gmail.com>
2023-10-31 14:41:38 +0900

    [Feat] user profile, records data available

---
+++ Assets/Firebase/RecordManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class RecordManager : MonoBehaviour
+{
+    // singleton
+    private static RecordManager instance;
+
+    // firebase
+    private FirebaseManager.Profile userProfile;
+    private Dictionary<string, FirebaseManager.Record> userRecords;
+
+    public static RecordManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirebaseManager.Instance.InitializeFirebase();
+    }
+
+    public void GetProfile(FirebaseManager.Profile profile)
+    {
+        userProfile = profile;
+    }
+
+    public void GetRecords(Dictionary<string, FirebaseManager.Record> records)
+    {
+        userRecords = records;
+    }
+
+    public FirebaseManager.Profile UserProfile
+    {
+        get { return userProfile; }
+    }
+
+    public Dictionary<string, FirebaseManager.Record> UserRecords
+    {
+        get { return userRecords; }
+    }
+}
