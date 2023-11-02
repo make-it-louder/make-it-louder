3600bd33a474985c3a0f45b4fd798c62ad0fb687
whitw7876@gmail.com <shkim7876@naver.com>
2023-11-02 17:59:04 +0900

    [Feat] Create room and Play

---
+++ Assets/InGameManager.cs
@@ -0,0 +1,43 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InGameManager : MonoBehaviour
+{
+    GameObject spawnedPlayer;
+    public GameObject playerPrefab;
+    public Vector3 spawnPoint;
+    void Start()
+    {
+        if (!PhotonNetwork.IsConnectedAndReady)
+        {
+            return;
+        }
+
+        spawnedPlayer = PhotonNetwork.Instantiate(
+            $"player/{playerPrefab.name}",
+            spawnPoint + new Vector3(Random.Range(-2.5f, 2.5f), Random.Range(-0.5f, 0.5f), 0),
+            Quaternion.identity
+        );
+        GridCamera2D camera = GameObject.Find("Main Camera").GetComponent<GridCamera2D>();
+        camera.follows = spawnedPlayer;
+        spawnedPlayer.GetComponent<PlayerNickname>().SetNickname(PhotonNetwork.LocalPlayer.NickName);
+
+        SoundEventManager soundEventManager = GameObject.Find("SoundEventManager").GetComponent<SoundEventManager>();
+        spawnedPlayer.GetComponentInChildren<MicInputManager>().SoundEventManager = soundEventManager;
+
+        PlayerMove2D playerMoveBehavior = spawnedPlayer.GetComponent<PlayerMove2D>();
+
+        TMP_Text jumpCountText = GameObject.Find("JumpCount").GetComponent<TMP_Text>();
+        TMP_Text playTimeText = GameObject.Find("PlayTime").GetComponent<TMP_Text>();
+        PlayerMove2D playerMove2D = spawnedPlayer.GetComponent<PlayerMove2D>();
+
+        GameObject.Find("ChatManager").GetComponent<ChatManager>().playerMove2D = playerMove2D;
+
+        playerMoveBehavior.jumpCountText = jumpCountText;
+        playerMoveBehavior.playTimeText = playTimeText;
+    }
+
+}
