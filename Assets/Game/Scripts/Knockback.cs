e9dc74b1a20bfa56ab1b7ffaff5e5308ca8e55f7
dyddnals@naver.com <dyddnals@naver.com>
2023-11-03 15:06:10 +0900

    [tmp]

--- Assets/Game/Scripts/Knockback.cs
+++ Assets/Game/Scripts/Knockback.cs
@@ -6,10 +6,12 @@ public class Knockback : MonoBehaviour
     public float XStrength = 10f;  // 밀려나는 힘의 크기를 설정합니다.
     public float YStrength = 10f;  // 밀려나는 힘의 크기를 설정합니다.
     PlayerMove2D playerMove2D;
+    BlinkingObject blinkingObject;
     IEnumerator ResetIgnoreInput()
     {
         yield return new WaitForSeconds(1f);  // 예를 들어, 0.5초 동안 입력을 무시합니다.
         playerMove2D.IgnoreInput = false;
+        blinkingObject.StopBlinking();
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
@@ -19,8 +21,10 @@ public class Knockback : MonoBehaviour
         {
             // 플레이어의 Rigidbody2D 컴포넌트를 가져옵니다.
             Rigidbody2D playerRb = collider.gameObject.GetComponent<Rigidbody2D>();
+            blinkingObject = collider.gameObject.GetComponent<BlinkingObject>();
             playerMove2D = collider.gameObject.GetComponent<PlayerMove2D>();
             if (playerMove2D.IgnoreInput == true) return;
+            blinkingObject.StartBlinking();
             playerMove2D.IgnoreInput = true;
             StartCoroutine(ResetIgnoreInput());
 
