de1487749d94e7d8d8cf6ae886896cc5e3c7b69f
BestDiver <limcs1998@naver.com>
2023-11-05 20:37:38 +0900

    Feat - nickname

--- Assets/GameUI/Script/ChangeNick.cs
+++ Assets/GameUI/Script/ChangeNick.cs
@@ -6,10 +6,26 @@ using UnityEngine;
 public class ChangeNick : MonoBehaviour
 {
     public TMP_InputField cn;
+    public TMP_Text nick;
+    public GameObject inputFieldObject;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        HideInputField(); // 시작할 때 입력 필드를 숨깁니다.
+    }
+
+
+    public void HideInputField()
+    {
+        inputFieldObject.SetActive(false);
+
+    }
+
+    public void ShowInputField()
+    {
+        inputFieldObject.SetActive(true);
     }
 
     // Update is called once per frame
@@ -21,5 +37,7 @@ public class ChangeNick : MonoBehaviour
     public async void UpdateNick()
     {
         await RecordManager.Instance.UpdateUsername(cn.text);
+        nick.text = cn.text;
+
     }
 }
