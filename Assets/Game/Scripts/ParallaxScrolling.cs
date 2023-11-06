2d3f927e5a49420e89d55ea8da65e20d0ed5802e
dyddnals@naver.com <dyddnals@naver.com>
2023-11-06 14:38:31 +0900

    [fix] delete lerp in parallaxScrolling

--- Assets/Game/Scripts/ParallaxScrolling.cs
+++ Assets/Game/Scripts/ParallaxScrolling.cs
@@ -33,14 +33,14 @@ public class ParallaxScrolling : MonoBehaviour
     }
 
     // 프레임마다 호출되는 함수
-    void Update()
+    void FixedUpdate()
     {
         // 각 배경에 대해 반복
         for (int i = 0; i < backgrounds.Length; i++)
         {
             // 가로 파럴랙스는 반대 방향으로 카메라의 움직임에 비례하여 움직임
             float parallaxX = (previousCamPos.x - cam.position.x) * parallaxScales[i];
-            float parallaxY = (previousCamPos.y - cam.position.y) * parallaxScales[i] * verticalParallaxScale; // 세로 파럴랙스 추가
+            float parallaxY = (previousCamPos.y - cam.position.y) * parallaxScales[i]; // 세로 파럴랙스 추가
 
             // 대상 x, y 위치를 설정
             float backgroundTargetPosX = backgrounds[i].position.x + parallaxX;
@@ -50,7 +50,7 @@ public class ParallaxScrolling : MonoBehaviour
             Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgroundTargetPosY, backgrounds[i].position.z);
 
             // 부드러운 움직임을 위한 보간
-            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+            backgrounds[i].position = backgroundTargetPos;
         }
 
         // 이전 카메라 위치 업데이트
