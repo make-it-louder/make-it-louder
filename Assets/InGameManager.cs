74f1cc45b0bbeab4e1449fb31d947c9b0e577a6f
dyddnals@naver.com <dyddnals@naver.com>
2023-11-07 15:52:45 +0900

    [Feat] add 2nd background

--- Assets/InGameManager.cs
+++ Assets/InGameManager.cs
@@ -29,7 +29,7 @@ public class InGameManager : MonoBehaviour
         yield return new WaitUntil(() => PhotonNetwork.InRoom);
         spawnedPlayer = PhotonNetwork.Instantiate(
               $"player/{playerPrefab.name}",
-              spawnPoint + new Vector3(Random.Range(-2.5f, 2.5f), Random.Range(-0.5f, 0.5f), 0),
+              spawnPoint + new Vector3(Random.Range(-2.5f, 2.5f), Random.Range(-0.5f, 0.5f), -9.05f),
               Quaternion.identity
           );
         GridCamera2D camera = GameObject.Find("Main Camera").GetComponent<GridCamera2D>();
