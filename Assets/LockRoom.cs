3b63a2320257c8ed8033dd9f9a0323260aba15bc
BestDiver <limcs1998@naver.com>
2023-11-09 14:14:19 +0900

    .

--- Assets/LockRoom.cs
+++ Assets/LockRoom.cs
@@ -7,7 +7,13 @@ public class LockRoom : MonoBehaviour
 {
     public Room Room;
     public TMP_InputField inputField;
+    public TMP_Text wrongPasswordText; // 비밀번호가 틀렸을 때 표시할 텍스트
     public LobbyManager lobbyManager;
+
+    private void Start()
+    {
+        wrongPasswordText.gameObject.SetActive(false); // 초기에는 경고 텍스트를 숨깁니다.
+    }
 
     public void OnClickButton()
     {
@@ -17,14 +23,19 @@ public class LockRoom : MonoBehaviour
             Debug.LogError("Room not set yet");
             return;
         }
+
         bool ok = Room.Check(text);
         if (ok)
         {
+            // 비밀번호가 맞으면 경고 텍스트를 비활성화하고 방에 참여합니다.
+            wrongPasswordText.gameObject.SetActive(false);
             lobbyManager.JoinRoom(Room.RoomName);
         }
         else
         {
+            // 비밀번호가 틀리면 경고 텍스트를 활성화합니다.
             Debug.Log("Wrong password");
+            wrongPasswordText.gameObject.SetActive(true);
         }
     }
 }
