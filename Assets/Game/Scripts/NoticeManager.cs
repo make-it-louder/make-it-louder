268dc711c6f2a56e61a3a434278220d3887fc32c
dyddnals@naver.com <dyddnals@naver.com>
2023-11-14 17:23:04 +0900

    [Hotfix]

--- Assets/Game/Scripts/NoticeManager.cs
+++ Assets/Game/Scripts/NoticeManager.cs
@@ -1,7 +1,6 @@
 using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.PackageManager.UI;
 using UnityEngine;
 
 public class NoticeManager : MonoBehaviour
